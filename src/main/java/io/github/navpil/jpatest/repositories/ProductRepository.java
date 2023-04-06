fcc4b0ea64d750b41499d995efd1352ac44217e6
navpil <dima1715@gmail.com>
2023-04-06 11:23:55 +0300

    Update dependencies for it to work on latest JRE

--- src/main/java/io/github/navpil/jpatest/repositories/ProductRepository.java
+++ src/main/java/io/github/navpil/jpatest/repositories/ProductRepository.java
@@ -9,22 +9,22 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
- * This is the Data Access layer. Simple huh?
+ * This is the Data Access layer.
  * PLease note that no need for any dao implementation. This is an interface!
  */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    /** No need to define findAll() here, because
+    /* No need to define findAll() here, because
      *  inherited from JpaRepository with many other basic JPA operations.**/
     //public List<Product> findAll();
 
     /** spring-jpa-data understands this method name,
      *  because it supports the resolution of specific keywords inside method names. **/
-    public List<Product> findByNameContainingIgnoreCase(String searchString);
+    List<Product> findByNameContainingIgnoreCase(String searchString);
 
     /** You can define a JPA query.**/
     @Query("select p from Product p where p.name = :name")
-    public List<Product> findByNameIs(@Param("name") String name);
+    List<Product> findByNameIs(@Param("name") String name);
 
 }
